4f48ed3fce7f38fff018eaf2ec5d0e29dc1bec6c
Ari Putra <ariteknologi@gmail.com>
2019-12-15 16:54:18 +0700

    recreate app.vue |

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import appConfig from '@/app.config'
+
+export default {
+  name : 'app',
+  page: {
+    // All subcomponent titles will be injected into this template.
+    titleTemplate(title) {
+      title = typeof title === 'function' ? title(this.$store) : title
+      return title ? `${title} | ${appConfig.title}` : appConfig.title
+    },
+  },
+}
+</script>
+
+<!-- This should generally be the only global CSS in the app. -->
+<style lang="scss">
+@import '@scss';
+</style>
