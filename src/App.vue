864f693aa456f21eb40822da010c55f9d8014e0d
Ari Putra <ariteknologi@gmail.com>
2019-12-15 16:50:52 +0700

    fix minor

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 import appConfig from '@/app.config'
 
 export default {
+  name : 'app',
   page: {
     // All subcomponent titles will be injected into this template.
     titleTemplate(title) {
--- src/main.js
+++ src/main.js
@@ -6,15 +6,6 @@ import '@components/_globals'
 import VueTables from 'vue-tables-2'
 import BootstrapVue from 'bootstrap-vue'
 
-// Don't warn about using the dev version of Vue in development.
-Vue.config.productionTip = process.env.NODE_ENV === 'production'
-
-// If running inside Cypress...
-if (process.env.VUE_APP_TEST === 'e2e') {
-  // Ensure tests fail when Vue emits an error.
-  Vue.config.errorHandler = window.Cypress.cy.onUncaughtException
-}
-
 Vue.use(BootstrapVue)
 Vue.use(VueTables.ClientTable);
 
