1b5f809a1f02d85e9b887cfee105a6712b2065c4
Ari Putra <ariteknologi@gmail.com>
2020-01-09 01:54:12 +0700

    add admin crud

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import router from '@router'
 import '@components/_globals'
 import VueTables from 'vue-tables-2'
 import BootstrapVue from 'bootstrap-vue'
+import VeeValidate from 'vee-validate'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const options = {
+  confirmButtonColor: '#41b882',
+  cancelButtonColor: '#ff7674'
+}
 
 Vue.use(BootstrapVue)
 Vue.use(VueTables.ClientTable);
+Vue.use(VeeValidate)
+Vue.use(VueSweetalert2, options)
 
 const app = new Vue({
   router,
--- src/views/admin/list.vue
+++ src/views/admin/list.vue
@@ -1,7 +1,7 @@
 <script>
 import Layout from '@layouts/main'
 import Add from './add'
-import { get } from '@api'
+import { get,post,destroy } from '@api'
 
 export default {
     name : 'list',
@@ -22,6 +22,51 @@ export default {
             .catch((err) => {
                 console.log(err)
             })
+        },
+        edit(id) {
+            console.log(id)
+            post('api/auth/find', { id : id})
+            .then((res) => {
+                this.user = res.data.data
+                this.$bvModal.show('modal')
+                this.method = 'update'
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        },
+        destroy(id) {
+            this.$swal({
+                title : 'Are You Sure?',
+                text : "You won't able to revert this!",
+                type : "warning",
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.value) {
+                        destroy('api/auth/delete', { id : id})
+                        .then((res) => {
+                            this.$bvToast.toast(res.data.message, {
+                                title: 'Success',
+                                autoHideDelay: 5000,
+                                toaster : 'b-toaster-top-right',
+                                appendToast: false,
+                                variant : 'success'
+                            })
+                        })
+                        .catch((error) => {
+                            this.$bvToast.toast(error.response.data.message, {
+                                title: 'Error',
+                                autoHideDelay: 5000,
+                                toaster : 'b-toaster-top-right',
+                                appendToast: false,
+                                variant : 'error'
+                            })
+                        })
+                    }
+                })
         }
     },
     mounted() {
@@ -29,7 +74,7 @@ export default {
     },
     data() {
         return {
-            columns: ['name', 'email', 'phone'],
+            columns: ['name', 'email', 'phone','action'],
             rows: [],
             options: {
                 headings: {
@@ -39,7 +84,9 @@ export default {
                 },
                 sortable: ['name', 'email','phone'],
                 filterable: ['name', 'email','phone']
-            }
+            },
+            user : null,
+            method : 'post'
         }
     }
 }
@@ -51,20 +98,26 @@ export default {
             <div class="card-header">
                 <ul class="list-unstyled topnav-menu float-right mb-0"> 
                     <li class="d-none d-sm-block">
-                        <b-button v-b-modal.add variant="primary">Add New Admin</b-button>
+                        <b-button v-b-modal.modal variant="primary">Add New Admin</b-button>
                     </li>
                 </ul>
             </div>
             <div class="card-body">
-                <v-client-table :columns="columns" :data="rows" :options="options">
+                <v-client-table :columns="columns" :data="rows" :options="options" ref="table">
 
-                    <div slot="uri">
-                        <span>test</span>
+                    <div slot="action" slot-scope="props">
+                        <button type="button" class="btn btn-warning btn-icon edit" @click="edit(props.row.id)">
+                            <i class="fas fa-pencil-alt mg-r-0"></i>
+                        </button>
+
+                        <button type="button" class="btn btn-danger btn-icon delete" @click="destroy(props.row.id)">
+                            <i class="fas fa-trash mg-r-0" style="color : #fff !important"></i>
+                        </button>
                     </div>
 
                 </v-client-table>
             </div>
         </div>
-        <Add />
+        <Add v-bind:method="method" v-bind:data="user" />
     </Layout>
 </template>
