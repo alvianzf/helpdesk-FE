8264cd8a58bc6fd80509e53b9fa2d7d32f3d067b
Ari Putra <ariteknologi@gmail.com>
2020-02-12 00:45:25 +0700

    remove old template

--- src/utils/AuthRequired.js
+++
@@ -1,9 +0,0 @@
-export default (to, from, next) => {
-  if (localStorage.getItem('token') != null) {
-    // verify with firebase or jwt
-    next()
-  } else {
-    localStorage.removeItem('token')
-    next('/user/login')
-  }
-}
