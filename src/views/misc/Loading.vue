918c0e8ae3d19e50ecf1e7a8827fc4f9c4b781e9
Ari Putra <ariteknologi@gmail.com>
2020-02-12 01:50:12 +0700

    add loading component

---
+++ src/views/misc/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+ <loading
+     :show="loading"
+     :label="label">
+ </loading>
+</template>
+
+<script>
+import loading from 'vue-full-loading'
+
+export default {
+    components: {
+        loading
+    },
+    props : ['loading'],
+    data(){
+        return {
+            label: 'Loading...'
+        }
+    }
+}
+</script>
