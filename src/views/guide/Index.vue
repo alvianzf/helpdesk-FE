12a943eb41896845fd418aaf77fb58bd697e4c12
Ari Putra <ariteknologi@gmail.com>
2020-02-16 14:05:48 +0700

    fix minor

--- src/views/guide/Index.vue
+++ src/views/guide/Index.vue
@@ -7,9 +7,9 @@
                     <p> <strong> Step 1 </strong> </p>
                     <p> Copy this link in header tag <code class="highlighter-rouge">&lt;link rel="stylesheet" href="https://res.cloudinary.com/dtjnr7mrl/raw/upload/widget/css/app.css"&gt;</code> </p>
                     <p> <strong> Step 2 </strong> </p>
-                    <p> Copy this to below body tag <code class="highlighter-rouge"> &lt;script src="https://res.cloudinary.com/dtjnr7mrl/raw/upload/widget/js/app.js"&gt;&lt;/script&gt;</code> </p>
+                    <p> Copy this to above &lt;/body&gt; tag <code class="highlighter-rouge"> &lt;script src="https://res.cloudinary.com/dtjnr7mrl/raw/upload/widget/js/app.js"&gt;&lt;/script&gt;</code> </p>
                     <p> <strong> Step 3 </strong> </p>
-                    <p> Put this tag into body  <code class="highlighter-rouge">&lt;chat-widget web_name="Hongkong Pools" web_ip="178.128.93.246"/&gt;&lt;/code&gt;</code></p>
+                    <p> Put this tag into body  <code class="highlighter-rouge">&lt;chat-widget web_name="Hongkong Pools" web_ip="178.128.93.246"/&gt;&lt;/chat-widget&gt;</code></p>
                     <p> web_name is your destination web name, web_ip is your destination web ip </p>
                 </b-card>
             </b-col>
