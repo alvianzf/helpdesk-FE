fe211daf88b2e2d95c17fd1c7870807e4697bf3a
Ari Putra <ariteknologi@gmail.com>
2020-04-20 18:28:48 +0700

    fix minor

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -42,7 +42,7 @@
             </button>
           </div>
       </div>
-      <Modal ref="notifChatDetail" :id="selected" :key="selected"/>
+      <Modal ref="notifChatDetail" :id="selected"/>
   </div>
 </template>
 
