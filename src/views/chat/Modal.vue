5b2adc674e70ac520cbf3f1189e2a884c4eabc9f
Ari Putra <ariteknologi@gmail.com>
2020-04-21 10:27:58 +0700

    close modal on close chat

--- src/views/chat/Modal.vue
+++ src/views/chat/Modal.vue
@@ -282,12 +282,13 @@ export default {
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
             confirmButtonText: 'Yes, close it!'
-            }).then((result) => {
+            }).then(async (result) => {
                 if (result.value) {
-                    this.CLOSE_CHAT({
+                    await this.CLOSE_CHAT({
                         id : this.singlechat._id
                     })
-                    this.scrollToEnd()
+                    await this.scrollToEnd()
+                    await this.closeModal()
                 }
             })
         },
